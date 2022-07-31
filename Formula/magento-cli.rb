4fa7a96cf37743346c1f965b70cce809730f413b
goreleaserbot <goreleaser@blueacornici.com>
2022-07-30 21:36:15 -0400

    Brew formula update for magento-cli version v0.1.8

--- Formula/magento-cli.rb
+++ Formula/magento-cli.rb
@@ -5,21 +5,21 @@
 class MagentoCli < Formula
   desc "run magento natively with dockerized backend services"
   homepage "https://magento-cli.com"
-  version "0.1.7"
+  version "0.1.8"
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.intel?
-      url "https://github.com/blueacorninc/magento-cli/releases/download/v0.1.7/magento-cli_0.1.7_darwin_amd64.tar.gz"
-      sha256 "6d1bd15238c5091b58f7395706a344cc4048f8972f25eaa57aca3f6e5b5e2941"
+    if Hardware::CPU.arm?
+      url "https://github.com/blueacorninc/magento-cli/releases/download/v0.1.8/magento-cli_0.1.8_darwin_arm64.tar.gz"
+      sha256 "c7c5b013d1ce06f63b4c5d3f3447301cdb8ba0d179b89bde9ebb19a3712e7277"
 
       def install
         bin.install "magento"
       end
     end
-    if Hardware::CPU.arm?
-      url "https://github.com/blueacorninc/magento-cli/releases/download/v0.1.7/magento-cli_0.1.7_darwin_arm64.tar.gz"
-      sha256 "63706edea9c6e384a69f5286996f59fdb10b9b71c08c3eb6ad4cde95a63127d9"
+    if Hardware::CPU.intel?
+      url "https://github.com/blueacorninc/magento-cli/releases/download/v0.1.8/magento-cli_0.1.8_darwin_amd64.tar.gz"
+      sha256 "93e3aa866a2bcb314d9a7b759eabd928ce4cd9b3a285d3e10d6428bba610bfd9"
 
       def install
         bin.install "magento"
@@ -29,16 +29,16 @@ class MagentoCli < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/blueacorninc/magento-cli/releases/download/v0.1.7/magento-cli_0.1.7_linux_amd64.tar.gz"
-      sha256 "763a713ce8b1bf4a2db479002a1bbba33146d21c5a35bb31148a80aba2c74a32"
+      url "https://github.com/blueacorninc/magento-cli/releases/download/v0.1.8/magento-cli_0.1.8_linux_amd64.tar.gz"
+      sha256 "6c778bb2bd98f43d0eb480e4e7071a80b6595499d5c2a000d3aac9287f7daec6"
 
       def install
         bin.install "magento"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/blueacorninc/magento-cli/releases/download/v0.1.7/magento-cli_0.1.7_linux_arm64.tar.gz"
-      sha256 "968775c067a86a0857d9e2476cae2d0b106dcfe303337bea1b47788b412a7b7c"
+      url "https://github.com/blueacorninc/magento-cli/releases/download/v0.1.8/magento-cli_0.1.8_linux_arm64.tar.gz"
+      sha256 "1be192f25d27b84602e08825af8174efabfac112586730c807b87928f3884da1"
 
       def install
         bin.install "magento"
